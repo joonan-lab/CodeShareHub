a81bb73ea873cb9d7e4d0d421c946932356f7d2f
Chanwoong Hwa <54494981+woong4757@users.noreply.github.com>
2023-11-17 14:55:10 +0900

    Update testcode.R

--- testcode.R
+++ testcode.R
@@ -1,24 +1,22 @@
 library(dplyr)
 library(ggplot2)
-source("~/Desktop/run_enrichment_v2.2.R")
+source("run_enrichment_v3.2.R")
 
-data = readRDS("~/Downloads/DESeq2_table.RDS")
+data = readRDS("test-dataset/DESeq2_table.RDS")
 
+colnames(data)
 # runORA for up-DEGs
-res = run_ORA(data, 
-              df_source = "DESeq2", 
-              organism = "hsapiens", 
-              change = "pos")
+res = run_all(data, lfc_column = "log2FoldChange", 
+              padj_column = "padj", 
+              gene_name_column = 'gene_name', 
+              lfc_thresh = 0.25, 
+              padj_cutoff = 0.05, 
+              organism = 'human', 
+              minGSSize = 100, 
+              maxGSSize = 2000,
+              output_file = 'enrichment_result.xlsx')
 
-res2 = run_GSEA(data,
-                df_source = "DESeq2",
-                bg_file = "~/Downloads/h.all.v2022.1.Hs.symbols.gmt")
 
-plot_bar(res = res, 
-         isORA = T, n_term = 20, 
-         width = 7.5, height = 7.5, filename = "tmp.pdf")
+plot_volcano(res$DEGs)
 
-plot_bar(res = res2, 
-        isORA = F, n_term = 20, 
-        width = 7.5, height = 7.5, filename = "tmp2.pdf")
-
+plot_treeplot(res)
